2639afc6ef23878b788f42bfacbc279bdc8ae30a
Justin Harkey <justin.harkey@financialfinesse.com>
2020-04-08 23:59:33 -0700

    Fixes BEM naming of css properties, enhances OG tag parser function

--- src/Link.svelte
+++ src/Link.svelte
@@ -10,20 +10,34 @@
 	}
 
     function parseOpenGraphData(htmlData) {
-        const regex = /<meta(?=.*property="og:([^\"]*)")(?=.*content="([^\"]*)").*\/?>/gi;
-        // const regex = /property="(og:[^\"]*)"(?= content="([^\"]*)")/gi;
-        let matchesArray = [...htmlData.matchAll(regex)];
-		let ogTags = {};
-		if (matchesArray.length > 0) {
-			matchesArray.map((match) => {
-				ogTags[match[1]] = match[2];
-			});
+        // might be able to use Pocket API for all this
+        const metaTagRegex = /<meta([^>]*)\/?>/gi;
+        let metaTagMatches = [...htmlData.matchAll(metaTagRegex)];
+
+        let ogTags = {};
+        if (metaTagMatches.length > 0) {
+            metaTagMatches.map((metaTag) => {
+                if (metaTag[0].includes('og:')) {
+                    const propertySearch = /property="([^\"]*)"/gi;
+                    const contentSearch = /content="([^\"]*)"/gi;
+                    const key = propertySearch.exec(metaTag[0]);
+                    const value = contentSearch.exec(metaTag[0]);
+                    ogTags[key[1]] = value[1];
+                }
+            });
         }
+
+        if (!ogTags["og:title"]) {
+            const titleTagRegex = /<title>([^<]*)<\/title>/gi;
+            const titleTag = titleTagRegex.exec(htmlData);
+            ogTags["og:title"] = titleTag[1];
+        }
+
 		return ogTags;
 	}
     
     const getOpenGraphData = (async () => {
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`, { cache: 'force-cache' });
         const htmlData = await response.text();
         return await parseOpenGraphData(htmlData);
     })();
@@ -38,14 +52,14 @@
     border: 1px solid #ddd;
     border-radius: 3px;
 }
-.linkContainer {
+.link__container {
     background-repeat: no-repeat;
     background-size: 400px;
     padding: 14px 24px 14px 240px;
     position: relative;
     z-index: 0;
 }
-.linkContainer::after {
+.link__container::after {
     content: '';
     position: absolute;
     height: 100%;
@@ -56,50 +70,51 @@
     background-color: #fff;
     z-index: -1;
 }
-.linkTitle {
-    margin: 0 0 12px;
+.link__title {
+    margin: 0 0 4px;
     padding: 0;
-    font-size: 18px;
+    font-size: 16px;
     line-height: 1.3;
     color: #369;
 }
-.link:visited .linkTitle {
+.link:visited .link__title {
     color: darkslateblue;
 }
-.linkDescription {
-    margin: 0 0 12px;
+.link__description {
+    margin: 0 0 6px;
     padding: 0;
-    font-size: 14px;
+    font-size: 12px;
     line-height: 1.5;
 }
-.linkDate {
+.link__date {
     font-size: 10px;
     font-style: italic;
+    color: #555;
 }
 </style>
 
 <a class="link" href="{url}" target="_blank">
     {#await getOpenGraphData}
-        <div class="linkContainer">...loading open graph data</div>
+        <div class="link__container">...loading open graph data</div>
     {:then data}
-        <div class="linkContainer" style="background-image: url({data.image})">
-            <h1 class="linkTitle">
-                {#if data.title}
-                   {decodeHtmlEntities(data.title)}
+        <div class="link__container" style="background-image: url({data["og:image"]})">
+            <h1 class="link__title">
+                {#if data["og:title"]}
+                   {decodeHtmlEntities(data["og:title"])}
                 {:else}
                     {url}
                 {/if}
-                {#if data.site_name}
-                    - {data.site_name}
+                {#if data["og:site_name"]}
+                    - {data["og:site_name"]}
                 {/if}
             </h1>
-            {#if data.description}<p class="linkDescription">{decodeHtmlEntities(data.description)}</p>{/if}
-            <div class="linkDate">{formatDate(date)}</div>
+            {#if data["og:description"]}<p class="link__description">{decodeHtmlEntities(data["og:description"])}</p>{/if}
+            <div class="link__date">{formatDate(date)}</div>
         </div>
     {:catch error}
-        <div class="linkContainer">
-            <h1 class="linkTitle">{url}</h1>
-            <div class="linkDate">{formatDate(date)}</div>
+        <div class="link__container">
+            <h1 class="link__title">{url}</h1>
+            <div class="link__date">{formatDate(date)}</div>
         </div>
     {/await}
 </a>
